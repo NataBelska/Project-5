183461b37e30d2d7c86de846bfd27275c209ae49
NataBelska <natalija.belska@gmail.com>
2024-06-15 23:32:26 +0300

    my commit

--- cypress/e2e/cypress/e2e/issue-time-tracking_NB.cy.js
+++ cypress/e2e/cypress/e2e/issue-time-tracking_NB.cy.js
@@ -17,6 +17,7 @@ describe("Issue create", () => {
   const TypingTime = '[placeholder="Number"]';
   const CloseDetailsModal = '[data-testid="icon:close"]';
   const TimeLoggingWatch = '[data-testid="icon:stopwatch"]';
+  const TimeTrackingModal = '[data-testid="modal:tracking"]';
 
   //String variables:
   const DescriptionText =
@@ -60,15 +61,39 @@ describe("Issue create", () => {
     cy.get(DetailsModal)
       .should("be.visible")
       .within(() => {
-        cy.get(TypingTime).should("have.value", 20).clear().type("{enter}");
+        cy.get(TypingTime).should("have.value", 20).clear();
+        cy.get(TypingTime).should("not.have.value");
         cy.get(CloseDetailsModal).click();
       });
     cy.contains(TitleText).click();
     cy.get(DetailsModal).should("be.visible");
-    cy.get(TypingTime).should("not.have.value");
+    cy.get(TypingTime).should("not.have.value", 20);
+    cy.get(CloseDetailsModal).click();
+
+    //Checking time logging functionality
+    cy.contains(TitleText).click();
+    cy.get(TypingTime).type(10).type("{enter}").should("have.value", 10);
+    cy.get(TimeLoggingWatch).click();
+    cy.get(TimeTrackingModal)
+      .should("be.visible")
+      .within(() => {
+        cy.get(TypingTime).eq(0).type(2);
+        cy.get(TypingTime).eq(1).type(5);
+        cy.contains("button", "Done").click();
+      });
+    cy.get(TimeTrackingModal).should("not.exist");
+    cy.contains("2h logged").and("5h remaining").should("be.visible");
+
+    cy.get(TimeLoggingWatch).click();
+    cy.get(TimeTrackingModal)
+      .should("be.visible")
+      .within(() => {
+        cy.get(TypingTime).eq(0).clear();
+        cy.get(TypingTime).eq(1).clear();
+        cy.contains("button", "Done").click();
+      });
+    cy.get(TimeTrackingModal).should("not.exist");
+    cy.get(DetailsModal).should("be.visible");
+    cy.contains("No time logged").should("be.visible");
   });
-  /*Checking time logging functionality
-  cy.contains(TitleText).click();
-  cy.get(TypingTime).type(10).type("{enter}").should("have.value", 10);
-  cy.get(TimeLoggingWatch).parent().click();*/
 });
